1d3da373bd9cee348def9de2a7be50a675cd8ed6
codingbe <descuento@naver.com>
2022-03-24 15:37:30 +0900

    Update: 포트폴리오 사이트 1차 구현

--- projects.js
+++ projects.js
@@ -1,2 +1,35 @@
 const projects = document.querySelector(".projects");
-const projectsNav = projects.querySelector(".projects__category");
+const grid = projects.querySelector(".grid");
+
+const getProjects = async (type) => {
+  const { projects } = await (await fetch("data.json")).json();
+  grid.innerHTML = projects
+    .map((project) =>
+      project.type === type || type === "all" || !type
+        ? `
+    <div class="card" data-type=${project.type}>
+        <div class="card__info">
+            <h2 class="card__title">${project.name}</h2>
+            <p class="card__description">${project.description}</p>
+            <div class="card__icons">
+            <a href=${project.page} target="_blank"><i class="fab fa-chrome"></i></a>
+            <a href=${project.github} target="_blank"><i class="fab fa-github"></i></a>
+            </div>
+        </div>
+        <img src=${project.img} alt=${project.name} class="card__img" />
+    </div>
+`
+        : null
+    )
+    .join("");
+};
+
+projects.addEventListener("click", (e) => {
+  const className = e.target.className;
+  if (className === "projects__category") {
+    const type = e.target.innerText.toLowerCase();
+    getProjects(type);
+  }
+});
+
+getProjects();
